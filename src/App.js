c1a8e160970b34113749a7c8352c97b716ad059f
SphereSRYN <sphere.sryn@gmail.com>
2022-03-21 13:41:02 +0900

    중간커밋ㅣ

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    //const movies
+    // const {
+    //   data: {
+    //     data: { _movies },
+    //   },
+    // } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // this.setState({ movies: _movies });
+    // console.log("movies⛳", _movies.data.data.movies);
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies, isLoading: false }); //es6에서는 객체의 키와 대입할 변수의 이름이 같으면 코드를 축약할 수 있다.
+    console.log("movies⛳", movies.data.data.movies);
+  };
+
+  componentDidMount() {
+    //  axios.get({ url: "https://yts-proxy.now.sh/list_movies.json" });
+
+    this.getMovies();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return <h1>{isLoading ? "Loading ..." : "we re ready"}</h1>;
+  }
 }
-
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,77 @@
+//ctrl shift e 익스플로러 단축키
+import React from "react";
+import { checkPropTypes } from "prop-types";
+
+//Movie component는 state가 필요하지 않으므로 클래스형 컴포넌트가 아니라 함수형컴포넌트로 작성.
+function Movie() {
+  return <div>Movie</div>;
+}
+
+Movie.checkPropTypes = {
+  id: checkPropTypes.number.isRequired,
+  title: checkPropTypes.string.isRequired,
+  year: checkPropTypes.number.isRequired,
+  rating: checkPropTypes.number.isRequired,
+  genres: checkPropTypes.array.isRequired,
+  summary: checkPropTypes.string.isRequired,
+};
+
+/**_Movie =    {
+        "id": 40849,
+        "url": "https://yts.mx/movies/gamestop-rise-of-the-players-2022",
+        "imdb_code": "tt17200376",
+        "title": "Gamestop: Rise of the Players",
+        "title_english": "Gamestop: Rise of the Players",
+        "title_long": "Gamestop: Rise of the Players (2022)",
+        "slug": "gamestop-rise-of-the-players-2022",
+        "year": 2022,
+        "rating": 6.6,
+        "runtime": 94,
+        "genres": [
+          "Documentary"
+        ],
+        "summary": "From the makers of Console Wars comes the origin story of the Gamestop stock market phenomenon, featuring exclusive access to the original players who lit the fuse on a historic amateur investor uprising. Spotlighting the human side of a sensational business drama, Gamestop: Rise of the Players is a David vs Goliath tale about ordinary people waking up to the power they have in numbers. —Super LTD",
+        "description_full": "From the makers of Console Wars comes the origin story of the Gamestop stock market phenomenon, featuring exclusive access to the original players who lit the fuse on a historic amateur investor uprising. Spotlighting the human side of a sensational business drama, Gamestop: Rise of the Players is a David vs Goliath tale about ordinary people waking up to the power they have in numbers. —Super LTD",
+        "synopsis": "From the makers of Console Wars comes the origin story of the Gamestop stock market phenomenon, featuring exclusive access to the original players who lit the fuse on a historic amateur investor uprising. Spotlighting the human side of a sensational business drama, Gamestop: Rise of the Players is a David vs Goliath tale about ordinary people waking up to the power they have in numbers. —Super LTD",
+        "yt_trailer_code": "xeT6_kMi8Po",
+        "language": "en",
+        "mpa_rating": "",
+        "background_image": "https://yts.mx/assets/images/movies/gamestop_rise_of_the_players_2022/background.jpg",
+        "background_image_original": "https://yts.mx/assets/images/movies/gamestop_rise_of_the_players_2022/background.jpg",
+        "small_cover_image": "https://yts.mx/assets/images/movies/gamestop_rise_of_the_players_2022/small-cover.jpg",
+        "medium_cover_image": "https://yts.mx/assets/images/movies/gamestop_rise_of_the_players_2022/medium-cover.jpg",
+        "large_cover_image": "https://yts.mx/assets/images/movies/gamestop_rise_of_the_players_2022/large-cover.jpg",
+        "state": "ok",
+        "torrents": [
+          {
+            "url": "https://yts.mx/torrent/download/1DFF7ADE167B263BC714331F1759CC062E249E2D",
+            "hash": "1DFF7ADE167B263BC714331F1759CC062E249E2D",
+            "quality": "720p",
+            "type": "web",
+            "seeds": 0,
+            "peers": 0,
+            "size": "863.42 MB",
+            "size_bytes": 905361490,
+            "date_uploaded": "2022-03-20 21:48:47",
+            "date_uploaded_unix": 1647809327
+          },
+          {
+            "url": "https://yts.mx/torrent/download/E81133C30C57D0E5722DD052F584409C0E3D28BC",
+            "hash": "E81133C30C57D0E5722DD052F584409C0E3D28BC",
+            "quality": "1080p",
+            "type": "web",
+            "seeds": 0,
+            "peers": 0,
+            "size": "1.73 GB",
+            "size_bytes": 1857573356,
+            "date_uploaded": "2022-03-20 22:53:41",
+            "date_uploaded_unix": 1647813221
+          }
+        ],
+        "date_uploaded": "2022-03-20 21:48:47",
+        "date_uploaded_unix": 1647809327
+      },
+ * 
+ * 
+*/
+export default Movie;
